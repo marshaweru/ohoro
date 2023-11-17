03986b03ee5291028067314a15bbc87840357879
weru <147871343+marshaweru@users.noreply.github.com>
2023-11-17 09:10:06 -0800

    Minimum config to run the app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
 //Add a basic react functional component
+import React from 'react';
+
+const App = () => {
+    return {
+        <div>
+            <h1>Ohoro AI News App</h1>
+        </div>
+    };
+}
+
+//Above is minimum configuration to run the app
